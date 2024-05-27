f9b5ad6724f80b808461e98dbbead1132db353bc
Obaid Hamidoun <98986103+obaidHamidoun@users.noreply.github.com>
2024-05-27 13:11:06 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
-const sidebar = document.querySelector('.sidebar');
-const menuButton = document.querySelector('.menuButton');
+const menuButton = document.querySelector('.menuButton')
+const nav = document.querySelector('nav');
+const closeButton = document.querySelector('.closeButton');
 
-menuButton.addEventListener('click', function () {
-    sidebar.style = 'animation:slide 1s ease forwards';
+menuButton.addEventListener('click',function(){
+    nav.style = '    animation: slide 1s cubic-bezier(0.85, 0, 0.15, 1) forwards';
+})
+closeButton.addEventListener('click',function(){
+    nav.style = 'animation: Backslide 1s cubic-bezier(0.85, 0, 0.15, 1) forwards';
 })
