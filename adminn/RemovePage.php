60fb2a8078e85cbeb93a263369dda91b888ca6c2
Obaid Hamidoun <98986103+obaidHamidoun@users.noreply.github.com>
2024-06-04 16:55:23 +0100

    Create RemovePage.php

---
+++ adminn/RemovePage.php
@@ -0,0 +1,17 @@
+<?php
+
+ try{
+    $conn = new PDO("mysql:host=localhost;dbname=petshub",'root','');
+    $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+    $user_id = $_GET['id'];
+    $removeQuerry = "DELETE FROM users WHERE id = :user_id";
+    $stmt = $conn->prepare($removeQuerry);
+    $stmt->bindParam(':user_id' , $user_id);
+    $stmt->execute();
+    echo "<br><p class='errMessage'>tour removed succesfully</p>";
+    echo "<script>window.location.href= 'users.php'</script>";
+
+    }catch(PDOException $e){
+    echo "<script>alert('{$e->getMessage()}')</script>";
+    }
+   ?>
